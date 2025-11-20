45c0f992c74fa60b9b0865c1ad1655636f2e134a
TheBigDurlak <kacperd04@outlook.com>
2025-11-20 14:59:19 +0000

    added experimental background

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router'
+import './Home.css'
+import PsychedelicBackground from './background';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to Hendrix.chatbot</h1>
-            <Link to="/chat">Go to chat</Link>
+        <div className="home">
+            <PsychedelicBackground/>
+
+            <div className="home-content">
+                <h1 className="title" >Welcome to Hendrix.chatbot</h1>
+                <Link to="/chat">Go to chat</Link>
+            </div>    
         </div>
     );
 };
+
