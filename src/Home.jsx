8da200b18fb1d4c1c93c40624801379980cc41e2
TheBigDurlak <kacperd04@outlook.com>
2025-11-21 17:07:59 +0000

    changed home page styling, fixed component positioning

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,9 @@ import './Home.css';
 import Kaleido from './background.jsx';
 import UiWrapper from "./UIBox.jsx";
 import hendrix from "./img/hendrix.png";
+import crowd from "./img/crowd.png";
 import "./mainContainer.css";
+import "./UIBox.css"
 
 
 export default function Home() {
@@ -13,9 +15,35 @@ export default function Home() {
             <Kaleido/>
             <div className="main-container">
                 <img src={hendrix} className='hendrix-image' alt="transparent simple image of jimi hendrix"/>
+                <img src={crowd} className='crowd-image' alt="transparent simple image of a crowd"/>
                 <UiWrapper>
-                    <h1 className="title" >Are you experienced?</h1>
-                    <Link to="/chat">Go to chat</Link>  
+                    <div className="title">
+                        <svg width="500" height="175">
+                            <defs>
+                            <path
+                                id="top-arc"
+                                d="
+                                M 50 200
+                                A 150 130 0 0 1 450 215
+                                "
+                                fill="none"
+                            />
+                            </defs>
+
+                            <text
+                            fontSize="32"
+                            textAnchor="middle"
+                            fontFamily="Special Elite, serif"
+                
+                            >
+                            <textPath href="#top-arc" startOffset="50%">
+                                Are You Experienced?
+                            </textPath>
+                            </text>
+                        </svg>
+                        </div>
+
+                    <Link className="chat-button" to="/chat">Begin the Experience</Link>  
                 </UiWrapper>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Backbutton from"./Backbutton.jsx"
-import PsychedelicBackground from './background.jsx';
+import Kaleido from './background.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Backbutton /> 
     <App />
-    <PsychedelicBackground/>
+    <Kaleido/>
   </BrowserRouter>
 );
